42eeed75f55140f1e1fa93f9e901cd9836b8fd6d
Alexander Dahl <alex@netz39.de>
2017-11-29 13:59:00 +0100

    agent: sensors: Add loop over all detected chips
    So far just getting data aquisition to run.

--- agents/mqtt-sensors.c
+++ agents/mqtt-sensors.c
@@ -19,15 +19,30 @@
 
 int main( void )
 {
-    int ec;
+    char mystr[64];
+    int ec, i;
+    const sensors_chip_name *cn;
 
     ec = sensors_init( NULL );
     if ( ec )
     {
         (void) fprintf( stderr, "Error (%i) initializing libsensors!\n", ec );
+        goto leave;
+    }
+    (void) printf( "libsensors version %s found ...\n", libsensors_version );
+
+    i = 0;
+    while( (cn = sensors_get_detected_chips( NULL, &i )) )
+    {
+        if ( sensors_snprintf_chip_name( mystr, sizeof(mystr), cn ) < 0 )
+        {
+            break;
+        }
+
+        (void) printf( "chip %i: %s\n", i, mystr );
     }
 
+leave:
     sensors_cleanup();
-
     return ((ec == 0) ? EXIT_SUCCESS : EXIT_FAILURE);
 }
